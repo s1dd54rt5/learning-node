89c8e91b6ebad9dffecc3727111c1023b5b2290d
Siddharth Singh <siddharth.singh2019@vitstudent.ac.in>
2020-07-21 23:10:55 +0530

    checkedit

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const server = http.createServer((req,res)=>{
     console.log('request made');
 });
 
-server.listen(3000,'localhost',()=>{
-    console.log('listening for request on port 3000');
+server.listen(8000,'localhost',()=>{
+    console.log('listening for request on port 8000');
 });
 
-//Check it

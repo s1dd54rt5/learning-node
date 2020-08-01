9e7c8fabc415eb2611becefff1f4605657daa477
Siddharth Singh <siddharth.singh2019@vitstudent.ac.in>
2020-08-02 01:45:13 +0530

    path,url,redirects' '

--- server.js
+++ server.js
@@ -7,12 +7,35 @@ const server = http.createServer((req,res)=>{
     //set header content type
     
     res.setHeader('Content-Type','text/html');
-    fs.readFile('./views/index.html',(err,data)=>{
+
+    let path = './views/';
+    switch(req.url){
+        case '/':
+            path +='index.html';
+            res.statusCode=200;
+            break;
+        case '/about':
+            path +='about.html';
+            res.statusCode=200;
+            break;
+        case '/about-me':
+            res.statusCode=301;
+            res.setHeader('Location','/about');
+            res.end();
+            break;
+        default:
+            path +='404.html';
+            res.statusCode=404;
+            break;
+    }
+
+    fs.readFile(path,(err,data)=>{
         if(err){
             console.log(err);
             res.end();
         }else{
             //res.write(data);
+
             res.end(data);
         }
     })

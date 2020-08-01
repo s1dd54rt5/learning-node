aab85ab98bc6c10c1db118d8746a35a7803fffc6
Siddharth Singh <siddharth.singh2019@vitstudent.ac.in>
2020-08-02 01:02:33 +0530

    new

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
-    console.log(req);
+    console.log(req.url,req.method);
+
+    //set header content type
+    
+    res.setHeader('Content-Type','text/html');
+    fs.readFile('./views/index.html',(err,data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            //res.write(data);
+            res.end(data);
+        }
+    })
 });
 
 server.listen(8000,'localhost',()=>{
-    console.log('Hello World!');
     console.log('listening for request on port 8000');
 });
-

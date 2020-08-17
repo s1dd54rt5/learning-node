9f0713fefba57f2119f9be486e95e4ca77a6b34f
Siddharth Singh <siddharth.singh2019@vitstudent.ac.in>
2020-08-17 11:01:58 +0530

    lodash

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url,req.method);
 
+    // lodash
+    const num = _.random(0,20);
+    console.log(num);
     //set header content type
+
+    const greet = _.once(()=>{
+        console.log('hello');
+    })
+
+    greet();
+    greet();
     
     res.setHeader('Content-Type','text/html');
 
@@ -18,7 +28,7 @@ const server = http.createServer((req,res)=>{
             path +='about.html';
             res.statusCode=200;
             break;
-        case '/about-me':
+        case '/about-us':
             res.statusCode=301;
             res.setHeader('Location','/about');
             res.end();
